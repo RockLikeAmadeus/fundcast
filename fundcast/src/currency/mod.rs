d36226774ca6382cf6e5d3fc91943f9302cd6b5a
Alec Hampton <alhampton9@gmail.com>
2024-08-04 17:14:39 -0600

    Ideally we want to support primitive operators like +

--- fundcast/src/currency/mod.rs
+++ fundcast/src/currency/mod.rs
@@ -6,7 +6,8 @@ mod tests {
     fn adding_currency_values_gives_expected_result() {
         let two_dollars_fifty_one = Dollars::new(2, 51);
         let five_dollars_thirty_three = Dollars::new(5, 33);
-        let sum = two_dollars_fifty_one.plus(five_dollars_thirty_three);
+        // Probably use the *currencies* rust crate.
+        let sum = two_dollars_fifty_one + five_dollars_thirty_three;
         assert_eq(sum, Dollars::new(7, 84));
     }
 }
