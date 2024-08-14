e1fba3db5a872d3519c0d6fa8f5a3251d387e8d1
Alec Hampton <alhampton9@gmail.com>
2024-08-14 10:10:09 -0600

    Progress implementing Money type

--- fundcast-go/money/money.go
+++ fundcast-go/money/money.go
@@ -1,3 +1,11 @@
 package money
 
-type Money int
+type Money int64
+
+func (m Money) MajorPart() int64 {
+	return int64(m) / 100
+}
+
+func (m Money) MinorPart() int8 {
+	return int8(int64(m) % 100)
+}
--- fundcast-go/money/money_test.go
+++ fundcast-go/money/money_test.go
@@ -5,6 +5,7 @@ import (
 )
 
 func TestMoney(t *testing.T) {
+
 	t.Run("Test Money equality", func(t *testing.T) {
 		const errorMsg = "Equality for Money type is broken"
 		stack1 := Money(99)
@@ -18,10 +19,11 @@ func TestMoney(t *testing.T) {
 		}
 	})
 
-	// t.Run("Test zero value", func(t *testing.T) {
-	// 	cash := new(Money)
-	// 	assertCashValue(t, cash, Money(0))
-	// })
+	t.Run("Test zero value construction", func(t *testing.T) {
+		cash := *(new(Money))
+		assertCashValue(t, cash, 0)
+		assertCashValue(t, Money(12345), 12345)
+	})
 
 	// cash := new(Money)
 	// assert.Equal(cash.value_as_int64(), 0)
@@ -29,9 +31,31 @@ func TestMoney(t *testing.T) {
 	// assert.Equal(cash.minor_part(), 0)
 }
 
-// func assertCashValue(t testing.TB, expected, received *Money) {
-// 	t.Helper()
-// 	if expected != received {
-// 		t.Errorf("Expected value %d, received value %d")
-// 	}
-// }
+func assertCashValue(t testing.TB, received Money, expected int64) {
+	t.Helper()
+	expMajorPart := expected / 100
+	expMinorPart := int8(expected % 100)
+	recMajorPart := received.MajorPart()
+	recMinorPart := received.MinorPart()
+	if expected != int64(received) {
+		t.Errorf(
+			"Expected total money value %d, received value %d",
+			expected,
+			received,
+		)
+	}
+	if expMajorPart != recMajorPart {
+		t.Errorf(
+			"Expected major part of Money value %d, received value %d",
+			expMajorPart,
+			recMajorPart,
+		)
+	}
+	if expMinorPart != recMinorPart {
+		t.Errorf(
+			"Expected minor part of Money value %d, received value %d",
+			expMinorPart,
+			recMinorPart,
+		)
+	}
+}
