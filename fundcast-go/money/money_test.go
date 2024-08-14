7e8f364e52a8bd7bdfaa8c8cc7336a3c5c7c3788
Alec Hampton <alhampton9@gmail.com>
2024-08-14 11:33:08 -0600

    Implemented basic Money type

--- fundcast-go/money/money_test.go
+++ fundcast-go/money/money_test.go
@@ -6,7 +6,58 @@ import (
 
 func TestMoney(t *testing.T) {
 
-	t.Run("Test Money equality", func(t *testing.T) {
+	t.Run("Zero value Money construction works", func(t *testing.T) {
+		cash := *(new(Money))
+		assertCashValue(t, cash, 0)
+	})
+
+	t.Run("Normal Money construction works", func(t *testing.T) {
+		cash := Money(251)
+		assertCashValue(t, cash, 251)
+	})
+
+	// Placeholder: test constructing Money via string
+
+	t.Run("Adding Money works", func(t *testing.T) {
+		res := Money(251) + Money(533)
+		assertCashValue(t, res, 784)
+	})
+
+	t.Run("Subtracting Money works", func(t *testing.T) {
+		res := Money(533) - Money(251)
+		assertCashValue(t, res, 282)
+		res = Money(251) - Money(533)
+		assertCashValue(t, res, -282)
+	})
+
+	t.Run("Add-assigning Money works", func(t *testing.T) {
+		res := Money(251)
+		res += Money(533)
+		assertCashValue(t, res, 784)
+	})
+
+	t.Run("Subtract-Assigning Money works", func(t *testing.T) {
+		res := Money(251)
+		res -= Money(533)
+		assertCashValue(t, res, -282)
+	})
+
+	t.Run("Multiplying Money by integer works", func(t *testing.T) {
+		product := Money(251) * 99
+		assertCashValue(t, product, 24849)
+	})
+
+	t.Run("Dividing Money by integer gives expected result", func(t *testing.T) {
+		div := Money(252) / 2
+		assertCashValue(t, div, 126)
+	})
+
+	t.Run("Dividing Money by integer gives truncates", func(t *testing.T) {
+		div := Money(252) / 5
+		assertCashValue(t, div, 50)
+	})
+
+	t.Run("Money equality works", func(t *testing.T) {
 		const errorMsg = "Equality for Money type is broken"
 		stack1 := Money(99)
 		stack2 := Money(99)
@@ -19,16 +70,19 @@ func TestMoney(t *testing.T) {
 		}
 	})
 
-	t.Run("Test zero value construction", func(t *testing.T) {
-		cash := *(new(Money))
-		assertCashValue(t, cash, 0)
-		assertCashValue(t, Money(12345), 12345)
+	t.Run("Money comparison works", func(t *testing.T) {
+		const errorMsg = "Comparison for Money type is broken"
+		stack1 := Money(99)
+		stack2 := Money(100)
+		if stack1 > stack2 {
+			t.Errorf(errorMsg)
+		}
+		if stack2 < stack1 {
+			t.Errorf(errorMsg)
+		}
 	})
 
-	// cash := new(Money)
-	// assert.Equal(cash.value_as_int64(), 0)
-	// assert.Equal(cash.major_part(), 0)
-	// assert.Equal(cash.minor_part(), 0)
+	// Placeholder: test string formatting
 }
 
 func assertCashValue(t testing.TB, received Money, expected int64) {
